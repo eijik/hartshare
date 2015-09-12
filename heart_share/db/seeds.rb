e14d5442e9d825f813f80b1121f933cda181b68a
root <root@localhost.localdomain>
2015-09-12 17:39:02 +0900

    テストデータ修正

--- heart_share/db/seeds.rb
+++ heart_share/db/seeds.rb
@@ -6,6 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.new(name:'田中　太郎', image:open(Rails.root.join('db/images/img.jpeg')))
-User.new(name:'田中　花子', image:open(Rails.root.join('db/images/img2.jpeg')))
+# 田中家
+User.create(name:'田中　太郎', image:open(Rails.root.join('db/images/img.jpeg')))
+User.create(name:'田中　花子', image:open(Rails.root.join('db/images/img2.jpeg')))
+User.create(name:'田中　孝則')
+User.create(name:'田中　良子')
+
+# 陶山家
+User.create(name:'陶山　太郎', image:open(Rails.root.join('db/images/img.jpeg')))
+User.create(name:'陶山　花子', image:open(Rails.root.join('db/images/img2.jpeg')))
+User.create(name:'陶山　孝則')
+User.create(name:'陶山　良子')
+
+# 山本家
+User.create(name:'山本　太郎', image:open(Rails.root.join('db/images/img.jpeg')))
+User.create(name:'山本　花子', image:open(Rails.root.join('db/images/img2.jpeg')))
+User.create(name:'山本　孝則')
+User.create(name:'山本　良子')
 
