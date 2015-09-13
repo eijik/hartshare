d9fef442324c05a69d57c77d8048f494db5c8e8b
Eiji Kosaki <e_kosaki@galileoscope.co.jp>
2015-09-13 15:49:02 +0900

    いろいろ

--- heart_share/app/controllers/sleds_controller.rb
+++ heart_share/app/controllers/sleds_controller.rb
@@ -35,18 +35,25 @@ class SledsController < ApplicationController
     @sled.creator = User.first
 
     if @sled.save
-      redirect_to @sled, notice: 'Sled was successfully created.'
+      #redirect_to @sled, notice: '画像をアップしましたー'
+      #render :edit
+      redirect_to edit_sled_path(@sled), notice: '画像をアップしましたー'
     else
-      render :new
+      #render :new
+      redirect_to edit_sled_path(@sled), notice: '画像をアップしましたー'
+      #render :edit
     end
   end
 
   # PATCH/PUT /sleds/1
   def update
     if @sled.update(sled_params)
-      redirect_to @sled, notice: 'Sled was successfully updated.'
+      #redirect_to @sled, notice: '画像をアップしましたー'
+      #render :edit
+      redirect_to edit_sled_path(@sled), notice: '画像をアップしましたー'
     else
-      render :edit
+      #render :edit
+      redirect_to edit_sled_path(@sled), notice: '画像をアップしましたー'
     end
   end
 
@@ -64,6 +71,6 @@ class SledsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def sled_params
-      params.require(:sled).permit(:file)
+      params[:sled].present? ? params.require(:sled).permit(:file) : {}
     end
 end
